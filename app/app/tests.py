4b52d931acfcda6fed21a1e526b57b423c04bf66
silverag2002 <silverag2002@gmail.com>
2023-03-18 22:46:31 +0530

    Added databases

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,8 @@
-"""
-Sample Test
-"""
-
-
 from django.test import SimpleTestCase
-
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    def add_test(self):
+    def new_test(self):
         res = calc.add(6, 5)
         self.assertEqual(res, 11)
